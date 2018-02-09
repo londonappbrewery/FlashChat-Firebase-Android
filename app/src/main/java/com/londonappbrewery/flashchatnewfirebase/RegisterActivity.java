961cfc0ce1e86fe1265418f9fa677c6d91174791
londonappbrewery <londonappbrewery@gmail.com>
2018-02-09 13:53:56 +0000

    Updating password checking condition

--- app/src/main/java/com/londonappbrewery/flashchatnewfirebase/RegisterActivity.java
+++ app/src/main/java/com/londonappbrewery/flashchatnewfirebase/RegisterActivity.java
@@ -82,7 +82,7 @@ public class RegisterActivity extends AppCompatActivity {
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) || !isPasswordValid(password)) {
+        if (TextUtils.isEmpty(password) || !isPasswordValid(password)) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
             focusView = mPasswordView;
             cancel = true;
